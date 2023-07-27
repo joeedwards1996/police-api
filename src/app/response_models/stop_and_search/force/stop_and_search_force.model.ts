975dd1dfae444a03b97c66143682d0c245ed0f8b
joeedwards1996 <97850984+joeedwards1996@users.noreply.github.com>
2023-07-27 14:38:43 +0100

    added stop and seach force model

--- src/app/response_models/stop_and_search/force/stop_and_search_force.model.ts
+++ src/app/response_models/stop_and_search/force/stop_and_search_force.model.ts
@@ -17,7 +17,7 @@ export class StopAndSearchForce{
 
     public age_range: string;
     public self_defined_ethnicity: string;
-    public outcome_linked_to_object_of_search: boolean;
+    public outcome_linked_to_object_of_search: boolean | null; 
     public datetime: string;
     public removal_of_more_than_outer_clothing: boolean;
     public operation: boolean;
@@ -27,6 +27,49 @@ export class StopAndSearchForce{
     public gender: string; 
     public legislation: string; 
     public location: Location;
+    public outcome: boolean | string; 
+    public type: string; 
+    public operation_name: string | null;
+
+
+    constructor(age_range:string, 
+                self_defined_ethnocity:string,
+                outcome_linked_to_object_of_search: boolean | null,
+                datetime: string,
+                removal_of_more_than_outer_clothing: boolean,
+                operation: boolean,
+                officer_defined_ethnicity: string,
+                object_of_search: string,
+                involved_person: boolean,
+                gender: string,
+                legislation: string,
+                location: Location,
+                outcome: boolean | string,
+                type: string,
+                operation_name: string | null
+
+                ){
+                    this.age_range = age_range;
+                    this.self_defined_ethnicity = self_defined_ethnocity;
+                    this.outcome_linked_to_object_of_search = outcome_linked_to_object_of_search;
+                    this.datetime = datetime;
+                    this.removal_of_more_than_outer_clothing = removal_of_more_than_outer_clothing;
+                    this.operation = operation;
+                    this.officer_defined_ethnicity = officer_defined_ethnicity;
+                    this.object_of_search = object_of_search;
+                    this.involved_person = involved_person;
+                    this.gender = gender;
+                    this.legislation = legislation;
+                    this.location = location;
+                    this.outcome = outcome;
+                    this.type = type;
+                    this.operation_name = operation_name
+
+
+    }
+
+    
+
 
 
 
